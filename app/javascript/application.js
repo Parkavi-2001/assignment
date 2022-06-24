95058c7477d43f5df18ad058d439ebb3a6c747a3
Parkavi-2001 <108062390+Parkavi-2001@users.noreply.github.com>
2022-06-24 14:26:40 +0530

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //=require bootstrap.min.js
+//fyhdfjy
 import "@hotwired/turbo-rails"
 import "controllers"
